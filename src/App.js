9859d8c25376829cb147cbe41e19512bb9099f62
Bhavik Joshi <bhavikjoshi29@gmail.com>
2018-02-23 17:15:47 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+import { Header } from './components/common/index';
+
+class App extends Component {
+	render() {
+		return (
+			<View>
+				<Header headerText='Authentication App'/>
+				<Text>An App</Text>
+			</View>
+		)
+	}
+}
+
+export default App;
