f3c7d953a181230415cae33291ff06a2a0a40573
Bhavik Joshi <bhavikjoshi29@gmail.com>
2018-02-24 00:26:45 +0530

    Login Functionality Completed

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,43 @@
 import React, { Component } from 'react';
-import { Card, CardSection, Button, Input } from './common/index';
+import firebase from 'firebase';
+import { Text } from 'react-native';
+import { Card, CardSection, Button, Input, Spinner } from './common/index';
 
 class LoginForm extends Component {
-	state = { email: '', password: '' };
+	state = { email: '', password: '', error: '', loading: false };
+
+	onButtonPress() {
+		const { email, password } = this.state;
+
+		this.setState({ error: '', loading: true });
+
+		firebase.auth().signInWithEmailAndPassword(email, password)
+			.then(() => {
+				this.setState({ email: '', password: '', loading: false, error: '' })
+			})
+			.catch(() => {
+				firebase.auth().createUserWithEmailAndPassword(email, password)
+					.then(() => {
+						this.setState({ email: '', password: '', loading: false, error: '' })
+					})
+					.catch(() => {
+						this.setState({ error: 'Authentication Failed', loading: false })
+					});
+			});
+	}
+
+	renderButton() {
+		if(this.state.loading) {
+			return <Spinner size='small' />
+		}
+
+		return(
+			<Button onPress={this.onButtonPress.bind(this)}>
+				Log In
+			</Button>
+		)
+	}
+
 	render() {
 		return(
 			<Card>
@@ -12,14 +47,21 @@ class LoginForm extends Component {
 				<CardSection>
 					<Input placeholder='password' value={this.state.password} onChangeText={password => this.setState({ password })} label='Password' secureTextEntry />
 				</CardSection>
+				<Text style={styles.errorStyle}>{this.state.error}</Text>
 				<CardSection>
-					<Button>
-						Log In
-					</Button>
+					{this.renderButton()}
 				</CardSection>
 			</Card>
 		)
 	}
 }
+
+const styles = {
+	errorStyle: {
+		fontSize: 20,
+		alignSelf: 'center',
+		color: 'red'
+	}
+};
 
 export default LoginForm;
